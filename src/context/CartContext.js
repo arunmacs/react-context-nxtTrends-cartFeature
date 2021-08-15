67cd275a5b2465c9c1d77a179f0bbf0d98c0dac1
arunmacs <aruncharymuniganti@gmail.com>
2021-08-15 08:21:12 +0000

    using react context in diff routes, added cart feature and deleting that

---
+++ src/context/CartContext.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const CartContext = React.createContext({
+  cartList: [],
+  addCartItem: () => {},
+  deleteCartItem: () => {},
+})
+
+export default CartContext
+
+// cartList: [
+//     {
+//       title: 'Product 1',
+//       brand: 'Brand Name',
+//       id: 1001,
+//       imageUrl:
+//         'https://assets.ccbp.in/frontend/react-js/sample-product-img.jpg',
+//       price: 760,
+//       quantity: 5,
+//     },
+//     {
+//       title: 'Product 2',
+//       brand: 'Brand Name',
+//       id: 1002,
+//       imageUrl:
+//         'https://assets.ccbp.in/frontend/react-js/sample-product-img.jpg',
+//       price: 760,
+//       quantity: 2,
+//     },
+//   ],
